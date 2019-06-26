fb2adb7a548e44a77c83b5e598b1819c1f79da75
Asbel <asbeln01@yahoo.com>
2019-06-26 00:02:20 -0500

    Worked on Displaying a list of Customers and Movies

--- MovieRental/Models/Customer.cs
+++ MovieRental/Models/Customer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
+
 namespace MovieRental.Models
 {
     public class Customer
---
+++ MovieRental/ViewModels/DetailsCustomerViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MovieRental.Models;
+
+namespace MovieRental.ViewModels
+{
+    public class DetailsCustomerViewModel : Controller
+    {
+        public List<Customer> Customers { get; set; }
+    }
+}
