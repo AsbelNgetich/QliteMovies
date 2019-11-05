b0174cfc1035484829143f20d698cd358d382f8c
Asbel <asbeln01@yahoo.com>
2019-11-05 04:56:56 -0600

    Made changes on Customer Form. Added FormViewModel

---
+++ MovieRentals/Controllers/api/CustomersController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MovieRentals.Models;
+
+namespace MovieRentals.Controllers.api
+{
+    public class CustomersController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        //GET /api/customers
+        public IEnumerable<Customer> GetCustomers()
+        {
+            return _context.Customers.ToList();
+        }
+
+        //GEt /api/customers/1
+
+        public Customer GetCustomers(int id)
+        {
+
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            return customer;
+        }
+
+    }
+}
